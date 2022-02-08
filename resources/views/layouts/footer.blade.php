ddb5c1c7ed5efe908c798fa2a56fcab8f66f916d
Miftahur23 <miftahur109970@gmail.com>
2022-02-08 23:54:40 +0600

    Added new footer blade

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+<div class="card m-1">
+    <img class="card-img-top border border-light" src="{{ url('/asset/serve.jpg') }}" width="100" height="100" style="padding-left: 200px; padding-right: 200px;">
+    <div class="card-body">
+      <h5 class="card-title text-center">Contact Management Team For Any Problem</h5>
+      <p class="card-text text-center"> <strong>Mail- </strong><a href="mailto:samiur.rahman19@gmail.com">
+        samiur.rahman19@gmail.com</a></p>
+      <p class="card-text text-center" >Phone- <a href="tel:017">01682169231</a></p>
+    </div>
+  </div>
